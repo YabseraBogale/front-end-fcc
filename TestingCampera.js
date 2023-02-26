f4cdfa51551a06aa4a9eebb7812cd25327043200
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-02-26 13:29:07 +0300

    Add files via upload

---
+++ TestingCampera.js
@@ -0,0 +1,14 @@
+function setup() {
+  var cnv =createCapture(VIDEO);;
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y).size(200,200);
+  
+  
+}
+
+function draw(){
+	
+	
+	
+	}
