c47be0d636290e60cf46d5ba30417e474541933a
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-02-26 18:02:03 +0300

    Add files via upload

---
+++ TestingCampera2.js
@@ -0,0 +1,12 @@
+// Initialize the Image Classifier method with MobileNet
+const classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
+// When the model is loaded
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
+
+// Make a prediction with a selected image
+classifier.classify('Yab.jpeg', (err, results) => {
+  console.log(results);
+});
