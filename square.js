92e495c999886ec4d12632ab2ac26369fb9bd725
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2023-08-18 20:03:55 +0300

    Add files via upload

--- square.js
+++ square.js
@@ -3,5 +3,23 @@
 window.addEventListener("keyup",logKey)
 
 function logKey(pressed){
-	console.log(pressed.code);
+		console.log(pressed.code);
+
 }
+
+
+function move(pressed){
+	let p=document.getElementById("p");
+	switch(pressed.code){
+		case "ArrowRight":
+			p.style.marginRight="10px"
+		case "ArrowLeft":
+			p.style.marginLeft=""
+		case "ArrowUp":
+			break
+		case "ArrowDown":
+			break
+	}
+}
+
+
